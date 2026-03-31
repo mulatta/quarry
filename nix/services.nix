dee1717dc836be36c0c127ff1a1910f427300435
mulatta <67085791+mulatta@users.noreply.github.com>
2026-04-01 01:02:57 +0900

    refactor(nix): refactor services

---
+++ nix/services.nix
@@ -0,0 +1,53 @@
+{ inputs, ... }:
+let
+  pgSocketDir = "/tmp/quarry-pg";
+in
+{
+  imports = [ inputs.process-compose-flake.flakeModule ];
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      process-compose.services = {
+        imports = [ inputs.services-flake.processComposeModules.default ];
+
+        services.postgres.quarry-pg = {
+          enable = true;
+          package = pkgs.postgresql_16;
+          dataDir = "./.pg-data";
+          socketDir = pgSocketDir;
+          settings = {
+            listen_addresses = "";
+            synchronous_commit = "off";
+            shared_buffers = "8GB";
+            work_mem = "256MB";
+            maintenance_work_mem = "2GB";
+            effective_io_concurrency = 200;
+            autovacuum = "off";
+          };
+          superuser = null;
+          initialDatabases = [
+            {
+              name = "quarry";
+              schemas = [ ../sql/schema.sql ];
+            }
+            { name = "dagster"; }
+          ];
+        };
+
+        services.clickhouse."quarry-ch" = {
+          enable = true;
+          port = 9000;
+          extraConfig = {
+            http_port = 8124;
+          };
+          initialDatabases = [
+            {
+              name = "quarry";
+              schemas = [ ../sql/ch_schema.sql ];
+            }
+          ];
+        };
+      };
+    };
+}
